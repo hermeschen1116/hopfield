4acc87ab55c8da5053d1ec84c2165f4e20a32b9d
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-12-10 22:51:52 +0800

    fix(src/hopfield/Network.py): correct Hopfield network

--- src/hopfield/Network.py
+++ src/hopfield/Network.py
@@ -1,23 +1,31 @@
 import numpy
 
 
+def signal_function(s: numpy.ndarray, x: numpy.ndarray) -> numpy.ndarray:
+	if x.shape != s.shape:
+		raise ValueError("signal_function: s and x should be in the same shape.")
+
+	return numpy.ones_like(s) * (s > 0) + numpy.ones_like(s) * -1 * (s < 0) + x * (s == 0)
+
+
 class HopfieldNetwork:
 	def __init__(self, input_size: int) -> None:
 		self.__input_size: int = input_size
 		self.__weight: numpy.ndarray = numpy.zeros((input_size, input_size))
+		self.__threshold: numpy.ndarray = numpy.zeros((input_size))
 
 	def memorize(self, patterns: numpy.ndarray) -> None:
 		for pattern in patterns:
 			self.__weight += numpy.outer(pattern, pattern)
 		numpy.fill_diagonal(self.__weight, 0)
+		self.__threshold = self.__weight.sum(0)
 
 	def retrieve(self, stimulate: numpy.ndarray, max_iter: int = 10) -> numpy.ndarray:
 		state: numpy.ndarray = stimulate.copy()
 		for iteration in range(max_iter):
 			previous_state: numpy.ndarray = state.copy()
-			for i in range(self.__input_size):
-				net_input = numpy.dot(self.__weight[i], state)
-				state[i] = 1 if net_input > 0 else -1
+			output: numpy.ndarray = state.dot(self.__weight) - self.__threshold
+			state = signal_function(output, state)
 			if numpy.array_equal(state, previous_state):
 				break
 
