2d70527781444ee56a34534baa6cf1de779f45ad
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-12-15 23:27:05 +0800

    fix(src/hopfield/Network.py): corrct memorize process

--- src/hopfield/Network.py
+++ src/hopfield/Network.py
@@ -1,11 +1,13 @@
 import numpy
 
+from hopfield.DataProcess import Dataset
 
-def signal_function(s: numpy.ndarray, x: numpy.ndarray) -> numpy.ndarray:
-	if x.shape != s.shape:
+
+def signal_function(output: numpy.ndarray, state: numpy.ndarray) -> numpy.ndarray:
+	if state.shape != output.shape:
 		raise ValueError("signal_function: s and x should be in the same shape.")
 
-	return numpy.ones_like(s) * (s > 0) + numpy.ones_like(s) * -1 * (s < 0) + x * (s == 0)
+	return numpy.ones_like(output) * (output > 0) + numpy.ones_like(output) * -1 * (output < 0) + state * (output == 0)
 
 
 class HopfieldNetwork:
@@ -14,9 +16,11 @@ class HopfieldNetwork:
 		self.__weight: numpy.ndarray = numpy.zeros((input_size, input_size))
 		self.__threshold: numpy.ndarray = numpy.zeros((input_size))
 
-	def memorize(self, patterns: numpy.ndarray) -> None:
-		for pattern in patterns:
-			self.__weight += numpy.outer(pattern, pattern)
+	def memorize(self, knowledgs: Dataset) -> None:
+		for knowledge in knowledgs:
+			self.__weight += numpy.outer(knowledge, knowledge)
+
+		self.__weight /= self.__weight.shape[0]
 		numpy.fill_diagonal(self.__weight, 0)
 		self.__threshold = self.__weight.sum(0)
 
@@ -24,7 +28,7 @@ class HopfieldNetwork:
 		state: numpy.ndarray = stimulate.copy()
 		for iteration in range(max_iter):
 			previous_state: numpy.ndarray = state.copy()
-			output: numpy.ndarray = state.dot(self.__weight) - self.__threshold
+			output: numpy.ndarray = state.T.dot(self.__weight) - self.__threshold
 			state = signal_function(output, state)
 			if numpy.array_equal(state, previous_state):
 				break
