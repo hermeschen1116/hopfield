4f2422a39839395e70e7af207c4c4f8bcf5cf04d
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-12-16 00:58:32 +0800

    perf(src/hopfield/Network.py): async update neuron

--- src/hopfield/Network.py
+++ src/hopfield/Network.py
@@ -7,11 +7,13 @@ def signal_function(output: numpy.ndarray, state: numpy.ndarray) -> numpy.ndarra
 	if state.shape != output.shape:
 		raise ValueError("signal_function: s and x should be in the same shape.")
 
-	return numpy.ones_like(output) * (output > 0) + numpy.ones_like(output) * -1 * (output < 0) + state * (output == 0)
+	return numpy.where(output > 0, 1, numpy.where(output < 0, -1, state))
 
 
 class HopfieldNetwork:
-	def __init__(self, input_size: int) -> None:
+	def __init__(self, input_size: int, seed: int = 42) -> None:
+		numpy.random.seed(seed)
+
 		self.__input_size: int = input_size
 		self.__weight: numpy.ndarray = numpy.zeros((input_size, input_size))
 		self.__threshold: numpy.ndarray = numpy.zeros((input_size))
@@ -19,17 +21,20 @@ class HopfieldNetwork:
 	def memorize(self, knowledges: Dataset) -> None:
 		for knowledge in knowledges:
 			self.__weight += numpy.outer(knowledge, knowledge)
-		self.__weight /= self.__weight.shape[0]
+		self.__weight /= self.__input_size
 		numpy.fill_diagonal(self.__weight, 0)
 
 		self.__threshold = self.__weight.sum(0)
 
-	def recall(self, stimulate: numpy.ndarray, max_iter: int = 10) -> numpy.ndarray:
+	def recall(self, stimulate: numpy.ndarray, max_iter: int = 100) -> numpy.ndarray:
 		state: numpy.ndarray = stimulate.copy()
 		for iteration in range(max_iter):
 			previous_state: numpy.ndarray = state.copy()
-			output: numpy.ndarray = numpy.sum(self.__weight * state, axis=1) - self.__threshold
-			state = signal_function(output, state)
+
+			for i in numpy.random.permutation(self.__input_size):
+				output_i = numpy.dot(self.__weight[i, :], state) - self.__threshold[i]
+				state[i] = signal_function(numpy.array([output_i]), numpy.array([state[i]]))[0]
+
 			if numpy.array_equal(state, previous_state):
 				print(f"Recall Iteration: {iteration + 1}")
 				break
