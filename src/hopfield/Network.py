eb7be475f651cb828508a723782f52728de6259e
hermeschen1116 <108386417+hermeschen1116@users.noreply.github.com>
2024-12-16 22:25:24 +0800

    feat(src/hopfield/Network.py): implement add_noise

--- src/hopfield/Network.py
+++ src/hopfield/Network.py
@@ -3,6 +3,15 @@ import numpy
 from hopfield.DataProcess import Dataset
 
 
+def add_noise(x: numpy.ndarray, threshold: float = 0) -> numpy.ndarray:
+	flip_mask: numpy.ndarray = numpy.random.rand(*x.shape) < threshold
+
+	flipped_x = x.copy()
+	flipped_x[flip_mask] *= -1
+
+	return flipped_x
+
+
 def signal_function(output: numpy.ndarray, state: numpy.ndarray) -> numpy.ndarray:
 	if state.shape != output.shape:
 		raise ValueError("signal_function: s and x should be in the same shape.")
@@ -11,16 +20,18 @@ def signal_function(output: numpy.ndarray, state: numpy.ndarray) -> numpy.ndarra
 
 
 class HopfieldNetwork:
-	def __init__(self, input_size: int, seed: int = 42) -> None:
+	def __init__(self, input_size: int, noise_rate: float = 0.3, seed: int = 42) -> None:
 		numpy.random.seed(seed)
 
 		self.__input_size: int = input_size
+		self.__noise_rate: float = noise_rate
 		self.__weight: numpy.ndarray = numpy.zeros((input_size, input_size))
 		self.__threshold: numpy.ndarray = numpy.zeros((input_size))
 
 	def memorize(self, knowledges: Dataset) -> None:
 		for knowledge in knowledges:
-			self.__weight += numpy.outer(knowledge, knowledge)
+			obscure_knowledge: numpy.ndarray = add_noise(knowledge, self.__noise_rate)
+			self.__weight += numpy.outer(obscure_knowledge, obscure_knowledge)
 		self.__weight /= self.__input_size
 		numpy.fill_diagonal(self.__weight, 0)
 
